290c2e25d41aeedaa092d5a146467c6f9046ca69
Maryam Hassan <mariam542000@gmail.com>
2024-10-19 01:33:48 +0300

    update-footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,4 +1,4 @@
-import { Box, Grid, Typography, Divider, IconButton } from "@mui/material";
+import { Box, Grid, Typography, Divider, IconButton, Container } from "@mui/material";
 import FacebookIcon from "@mui/icons-material/Facebook";
 import TwitterIcon from "@mui/icons-material/Twitter";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
@@ -20,162 +20,171 @@ function Footer() {
         mt: "auto",
       }}
     >
-      <Grid
-        container
-        justifyContent="space-between"
-        alignItems="center"
-        sx={{ px: 3 }}
-      >
-        <Grid item xs={12} md={6}>
-          <Typography variant="h6" component="div" sx={{ mx: 8 }}>
-            <img
-              src={logo}
-              alt="Logo"
-              style={{ height: "120px", width: "120px" }}
-            />
-          </Typography>
-        </Grid>
+      <Container>
         <Grid
-          item
-          xs={12}
-          md={6}
-          sx={{ textAlign: { xs: "center", md: "right" } }}
+          container
+          justifyContent="space-between"
+          alignItems="center"
+          sx={{ px: 3 }}
         >
-          <Box
-            sx={{
-              display: "flex",
-              alignItems: "center",
-              justifyContent: { xs: "center", md: "flex-end" },
-            }}
-          >
-            <Typography variant="h6" component="div" sx={{ mb: 0, mr: 1 }}>
-              <strong className="footer-info">Follow us:</strong>
+          <Grid item xs={12} md={6}>
+            <Typography variant="h6" component="div" sx={{ mx: 8 }}>
+              <img
+                src={logo}
+                alt="Logo"
+                style={{ height: "120px", width: "120px" }}
+              />
             </Typography>
-            <Box sx={{ display: "flex", gap: 1 }}>
-              <IconButton className="social-icon">
-                <FacebookIcon />
-              </IconButton>
-              <IconButton className="social-icon">
-                <TwitterIcon />
-              </IconButton>
-              <IconButton className="social-icon">
-                <LinkedInIcon />
-              </IconButton>
-              <IconButton className="social-icon">
-                <InstagramIcon />
-              </IconButton>
+          </Grid>
+          <Grid
+            item
+            xs={12}
+            md={6}
+            sx={{ textAlign: { xs: "center", md: "right" } }}
+          >
+            <Box
+              sx={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: { xs: "center", md: "flex-end" },
+              }}
+            >
+              <Typography variant="h6" component="div" sx={{ mb: 0, mr: 1 }}>
+                <strong className="footer-info">Follow us:</strong>
+              </Typography>
+              <Box sx={{ display: "flex", gap: 1 }}>
+                <IconButton className="social-icon">
+                  <FacebookIcon />
+                </IconButton>
+                <IconButton className="social-icon">
+                  <TwitterIcon />
+                </IconButton>
+                <IconButton className="social-icon">
+                  <LinkedInIcon />
+                </IconButton>
+                <IconButton className="social-icon">
+                  <InstagramIcon />
+                </IconButton>
+              </Box>
             </Box>
-          </Box>
+          </Grid>
         </Grid>
-      </Grid>
 
-      {/* Divider */}
-      <Divider
-        sx={{
-          width: "80%",
-          mx: "auto",
-          my: 3,
-          backgroundColor: "var(--Secondary-2)",
-        }}
-      />
+        <Divider
+          sx={{
+            width: "80%",
+            mx: "auto",
+            my: 3,
+            backgroundColor: "var(--Secondary-2)",
+          }}
+        />
 
-      {/* Paragraph Text */}
-      <Typography variant="body1" align="left" sx={{ px: 15, mb: 5 }}>
-        <span className="footer-info">
-          Specialists in providing high-class tours for <br />
-          those in need. Contact us.
-        </span>
-      </Typography>
+        <Typography variant="body1" align="left" sx={{ px: 15, mb: 5 }}>
+          <span className="footer-info">
+            Specialists in providing high-class tours for <br />
+            those in need. Contact us.
+          </span>
+        </Typography>
 
-      {/* Grid Section */}
-      <Grid
-        container
-        spacing={4}
-        sx={{ px: 3, textAlign: { xs: "center", md: "center" } }}
-      >
-        <Grid item xs={12} md={4}>
-          <Typography
-            variant="h6"
-            component="div"
-            sx={{ mb: 2, color: "var(--Secondary-1)" }}
-          >
-            Contact Info
-          </Typography>
-          <Box sx={{ display: "flex", alignItems: "center", mb: 2, mx: 12 }}>
-            <HomeIcon className="footer-contact" />
-            <Typography variant="body2" component="p" className="footer-span">
-              123 Main St, City, Country
+        <Grid
+          container
+          spacing={4}
+          sx={{ px: 3, textAlign: { xs: "center", md: "center" } }}
+        >
+          <Grid item xs={12} md={4}>
+            <Typography
+              variant="h6"
+              component="div"
+              sx={{ mb: 2, color: "var(--Secondary-1)" }}
+            >
+              Contact Info
             </Typography>
-          </Box>
-          <Box sx={{ display: "flex", alignItems: "center", mb: 2, mx: 12 }}>
-            <PhoneIcon className="footer-contact" />
-            <Typography variant="body2" component="p" className="footer-span">
-              +20 123 070 1443
+            <Box sx={{ display: "flex", alignItems: "center", mb: 2, mx: 12 }}>
+              <HomeIcon className="footer-contact" />
+              <Typography
+                variant="body2"
+                component="p"
+                className="footer-span"
+              >
+               Cairo,Egypt
+              </Typography>
+            </Box>
+            <Box sx={{ display: "flex", alignItems: "center", mb: 2, mx: 12 }}>
+              <PhoneIcon className="footer-contact" />
+              <Typography
+                variant="body2"
+                component="p"
+                className="footer-span"
+              >
+                +20 123 070 1443
+              </Typography>
+            </Box>
+            <Box sx={{ display: "flex", alignItems: "center", mb: 2, mx: 12 }}>
+              <EmailIcon className="footer-contact" />
+              <Typography
+                variant="body2"
+                component="p"
+                className="footer-span"
+              >
+                info@company.com
+              </Typography>
+            </Box>
+          </Grid>
+          <Grid item xs={12} md={4}>
+            <Typography
+              variant="h6"
+              component="div"
+              sx={{ mb: 2, color: "var(--Secondary-1)" }}
+            >
+              Categories
             </Typography>
-          </Box>
-          <Box sx={{ display: "flex", alignItems: "center", mb: 2, mx: 12 }}>
-            <EmailIcon className="footer-contact" />
-            <Typography variant="body2" component="p" className="footer-span">
-              info@company.com
+            <Typography variant="body2" component="p" sx={{ mb: 2 }}>
+              <span className="footer-span">Our Services</span>
             </Typography>
-          </Box>
+            <Typography variant="body2" component="p" sx={{ mb: 2 }}>
+              <span className="footer-span">About Us</span>
+            </Typography>
+            <Typography variant="body2" component="p" sx={{ mb: 2 }}>
+              <span className="footer-span">Contact Us</span>
+            </Typography>
+          </Grid>
+          <Grid item xs={12} md={4}>
+            <Typography
+              variant="h6"
+              component="div"
+              sx={{ mb: 2, color: "var(--Secondary-1)" }}
+            >
+              Our Company
+            </Typography>
+            <Typography variant="body2" component="p" sx={{ mb: 2 }}>
+              <span className="footer-span">Property for Rent</span>
+            </Typography>
+            <Typography variant="body2" component="p" sx={{ mb: 2 }}>
+              <span className="footer-span">Property for Sale</span>
+            </Typography>
+            <Typography variant="body2" component="p" sx={{ mb: 2 }}>
+              <span className="footer-span">Property for Buy</span>
+            </Typography>
+          </Grid>
         </Grid>
-        <Grid item xs={12} md={4}>
-          <Typography
-            variant="h6"
-            component="div"
-            sx={{ mb: 2, color: "var(--Secondary-1)" }}
-          >
-            Categories
-          </Typography>
-          <Typography variant="body2" component="p" sx={{ mb: 2 }}>
-            <span className="footer-span">Our Services</span>
-          </Typography>
-          <Typography variant="body2" component="p" sx={{ mb: 2 }}>
-            <span className="footer-span">About Us</span>
-          </Typography>
-          <Typography variant="body2" component="p" sx={{ mb: 2 }}>
-            <span className="footer-span">Contact Us</span>
-          </Typography>
-        </Grid>
-        <Grid item xs={12} md={4}>
-          <Typography
-            variant="h6"
-            component="div"
-            sx={{ mb: 2, color: "var(--Secondary-1)" }}
-          >
-            Our Company
-          </Typography>
-          <Typography variant="body2" component="p" sx={{ mb: 2 }}>
-            <span className="footer-span">Property for Rent</span>
-          </Typography>
-          <Typography variant="body2" component="p" sx={{ mb: 2 }}>
-            <span className="footer-span">Property for Sale</span>
-          </Typography>
-          <Typography variant="body2" component="p" sx={{ mb: 2 }}>
-            <span className="footer-span">Property for Buy</span>
-          </Typography>
-        </Grid>
-      </Grid>
 
-      {/* Divider */}
-      <Divider
-        sx={{
-          width: "80%",
-          mx: "auto",
-          my: 3,
-          backgroundColor: "var(--Secondary-2)",
-        }}
-      />
+        <Divider
+          sx={{
+            width: "80%",
+            mx: "auto",
+            my: 3,
+            backgroundColor: "var(--Secondary-2)",
+          }}
+        />
 
-      {/* Copyright Section */}
-      <Typography
-        variant="body2"
-        align="center"
-        sx={{ px: 3, color: "var(--Secondary-2)" }}
-      >
-        Property Hub © 2024 All Rights Reserved
-      </Typography>
+        <Typography
+          variant="body2"
+          align="center"
+          sx={{ px: 3, color: "var(--Secondary-2)" }}
+        >
+          Property Hub © 2024 All Rights Reserved
+        </Typography>
+      </Container>
     </Box>
   );
 }
